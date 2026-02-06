051347fc85ac49b8f84a62fdac407adae0b6eeec
rain <1766306104@qq.com>
2026-02-06 14:25:18 +0800

    DAY11 - 系统优化（并发优化 - AI调用并发）
    1. 新增前端文件夹。

--- src/main/java/com/yupi/yuaicodemother/YuAiCodeMotherApplication.java
+++ src/main/java/com/yupi/yuaicodemother/YuAiCodeMotherApplication.java
@@ -6,7 +6,9 @@ import org.mybatis.spring.annotation.MapperScan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication(exclude = {RedisEmbeddingStoreAutoConfiguration.class})
 @MapperScan("com.yupi.yuaicodemother.mapper")
 public class YuAiCodeMotherApplication {
--- src/main/java/com/yupi/yuaicodemother/common/BaseResponse.java
+++ src/main/java/com/yupi/yuaicodemother/common/BaseResponse.java
@@ -2,10 +2,12 @@ package com.yupi.yuaicodemother.common;
 
 import com.yupi.yuaicodemother.exception.ErrorCode;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
+@NoArgsConstructor
 public class BaseResponse<T> implements Serializable {
 
     private int code;
